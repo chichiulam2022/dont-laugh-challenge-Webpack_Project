d6ea2ddf4ebd6aa9729f88cf9c9d4ade67664d47
Chi Chiu Lam <lamcc90@gmail.com>
2022-11-06 22:32:21 -0500

    restructure generateJoke function

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,17 +1,37 @@
 import axios from 'axios'
 
-function generateJoke() {
-    const config = {
-        headers: {
-            Accept: 'application/json'
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json'
+//         }
+//     }
+//     axios.get('https://icanhazdadjoke.com', config)
+//         .then(res => {
+//             document.getElementById('joke').innerHTML = res.data.joke
+//             console.log(res);
+//         })
+
+// }
+
+
+const generateJoke = async () => {
+    try {
+        const config = {
+            headers: {
+                Accept: 'application/json'
+            }
         }
+        const res = await axios.get('https://icanhazdadjoke.com', config)
+        const joke = res.data.joke
+        document.getElementById('joke').innerHTML = joke
+        console.log(joke);
+
     }
-    axios.get('https://icanhazdadjoke.com', config)
-        .then(res => {
-            document.getElementById('joke').innerHTML = res.data.joke
-            console.log(res);
-        })
+    catch (err) {
+        console.log(err);
+    }
+
 
 }
-
 export default generateJoke
